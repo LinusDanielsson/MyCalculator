fe9c37d0a55e6c9b9794bfb5168df12e60a04560
Koisk <94958796+LinusDanielsson@users.noreply.github.com>
2022-02-01 09:19:11 +0100

    6
    Sub - button

--- JqueryApp.js
+++ JqueryApp.js
@@ -11,7 +11,6 @@ var calcArray = [];
 
 
 //Add(+) button function
-
 buttonAdd.on("click", function(){
 
     resultDisplayer.empty(); // .empty() som Tömmer vår result display.
@@ -26,6 +25,27 @@ buttonAdd.on("click", function(){
 
     else {
         calcDisplayer.append(" + " + textToNumber);
+    }
+
+    textInputField.val(""); //Tömmer inmatingsfältet
+
+});
+
+//Sub(-) button function
+buttonSub.on("click", function(){
+
+    resultDisplayer.empty(); // .empty() som Tömmer vår result display.
+
+    textToNumber = Number(textInputField.val()); // Gör om vår text till ett nummer value
+
+    calcArray.push(-textToNumber); // .push() för att få in det inmatade nummret till Array
+
+    if (calcArray.length == 1) {    //IF sats för att förhindra en operator från att visas först i vår calcDisplayer
+        calcDisplayer.append(textToNumber); 
+    }
+
+    else {
+        calcDisplayer.append(" - " + textToNumber);
     }
 
     textInputField.val(""); //Tömmer inmatingsfältet
