046dc818bc8dec074d08b90bb5134ab8a2f65112
Koisk <94958796+LinusDanielsson@users.noreply.github.com>
2022-02-01 09:14:24 +0100

    5
    + button function

--- JqueryApp.js
+++ JqueryApp.js
@@ -5,8 +5,29 @@ var buttonEval = $("#eval");
 var textInputField = $("#inputField");
 
 var calcDisplayer = $("#numDisplay");
-var resultDisplayer = $("calcResultDisplay");
+var resultDisplayer = $("#calcResultDisplay");
 
 var calcArray = [];
 
 
+//Add(+) button function
+
+buttonAdd.on("click", function(){
+
+    resultDisplayer.empty(); // .empty() som Tömmer vår result display.
+
+    textToNumber = Number(textInputField.val()); // Gör om vår text till ett nummer value
+
+    calcArray.push(textToNumber); // .push() för att få in det inmatade nummret till Array
+
+    if (calcArray.length == 1) {    //IF sats för att förhindra en operator från att visas först i vår calcDisplayer
+        calcDisplayer.append(textToNumber); 
+    }
+
+    else {
+        calcDisplayer.append(" + " + textToNumber);
+    }
+
+    textInputField.val(""); //Tömmer inmatingsfältet
+
+});
